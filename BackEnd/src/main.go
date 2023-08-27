142d4dac08dcccb21e4197aefc562e76e2eaed80
Vernik22 <3003160030101@ingenieria.usac.edu.gt>
2023-08-27 17:18:33 -0600

    Server Back
    Servidor backend

--- BackEnd/src/main.go
+++ BackEnd/src/main.go
@@ -1,7 +1,277 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	http.FileServer(http.Dir("public"))
+	log.Println("HOLA MUNDO")
+}
+
+type Codigo struct {
+	Nombre string
+}
+type TraduccionPy struct {
+	Errores    []string
+	Tokens     []string
+	ErroresSin []string
+	Traduccion string
+}
+
+func texto(w http.ResponseWriter, r *http.Request) {
+	var url = "http://localhost:3000/traduccion/recibir"
+	var decoder = json.NewDecoder(r.Body)
+	var c Codigo
+	err := decoder.Decode(&c)
+	log.Println(c.Nombre)
+	if err != nil {
+		panic(err)
+	}
+
+	var jsonStr = []byte(`{"Nombre":"` + c.Nombre + `"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(string(bodyBytes))
+	fmt.Fprintf(w, string(bodyBytes))
+	log.Println(decoder)
+}
+
+type TextoSwift struct {
+	Title string `json: "title"`
+	Data  string `json: "data"`
+}
+
+func tradSwift(w http.ResponseWriter, r *http.Request) {
+	var textnuevo TextoSwift
+	err := json.NewDecoder(r.Body).Decode(&textnuevo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(textnuevo.Data)
+
+	log.Println(textnuevo)
+	w.Write([]byte("Body recibido"))
+	/*
+		var url = "http://Py:3000/traduccion/recibir"
+		//var url = "http://localhost:3000/traduccion/recibir"
+
+		requestBody, err := json.Marshal(map[string]string{
+			"Nombre": texto,
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(requestBody))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println(string(body))
+	*/
+}
+func envioaJs(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf(r.FormValue("textarea0"))
+	r.ParseForm()
+	var texto = r.PostForm.Get("textarea0")
+
+	log.Println(texto)
+
+	var url = "http://Js:5000/traduccion/recibir"
+	//var url = "http://localhost:5000/traduccion/recibir"
+
+	requestBody, err := json.Marshal(map[string]string{
+		"Nombre": texto,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+
+}
+func errPy(w http.ResponseWriter, r *http.Request) {
+	var url = "http://Py:3000/traduccion/tradPy"
+	//var url = "http://localhost:3000/traduccion/tradPy"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+	fmt.Fprintf(w, string(body))
+
+	bytes := []byte(body)
+
+	var colores map[string]interface{}
+	//m := colores.()
+
+	if err := json.Unmarshal(bytes, &colores); err != nil {
+		panic(err)
+	}
+	fmt.Println(colores)
+	entries := strings.Split(string(body), "token")
+	fmt.Println(entries[0])
+
+}
+
+func errJs(w http.ResponseWriter, r *http.Request) {
+	var url = "http://Js:5000/traduccion/tradJs"
+	//var url = "http://localhost:5000/traduccion/tradJs"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+	fmt.Fprintf(w, string(body))
+
+	bytes := []byte(body)
+
+	var colores map[string]interface{}
+	//m := colores.()
+
+	if err := json.Unmarshal(bytes, &colores); err != nil {
+		panic(err)
+	}
+	fmt.Println(colores)
+	entries := strings.Split(string(body), "token")
+	fmt.Println(entries[0])
+
+}
+
+//--------------------------------------------------------------------------------------------
+
+func errPySin(w http.ResponseWriter, r *http.Request) {
+	//var url = "http://Py:3000/traduccion/Esin"
+	var url = "http://localhost:3000/traduccion/Esin"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+	fmt.Fprintf(w, string(body))
+
+}
+func tokens(w http.ResponseWriter, r *http.Request) {
+	//var url = "http://Py:3000/traduccion/Tokens"
+	var url = "http://localhost:3000/traduccion/Tokens"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+	fmt.Fprintf(w, string(body))
+
+}
+func cTraducido(w http.ResponseWriter, r *http.Request) {
+	//var url = "http://Py:3000/traduccion/Ctraducido"
+	var url = "http://localhost:3000/traduccion/Ctraducido"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+	fmt.Fprintf(w, string(body))
+
+}
 
 func main() {
-	fmt.Println("Hello World")
+	//go get -u github.com/gorilla/mux
+
+	r := mux.NewRouter()
+
+	//r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/texto", tradSwift).Methods("POST")
+	r.HandleFunc("/textoj", envioaJs).Methods("POST")
+	r.HandleFunc("/TradPy", errPy)
+	r.HandleFunc("/TradJs", errJs)
+
+	//r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("public"))))
+	log.Println("Escuchando en puerto8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
+
+func servirArchivos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		path := r.URL.Path
+
+		if path == "/" {
+			path = "./public/index.html"
+		} else {
+			path = "" + path
+		}
+		http.ServeFile(w, r, "./public/index.html")
+	case "POST":
+		r.ParseMultipartForm(0)
+		text := r.FormValue("archivo")
+		log.Println(text)
+	default:
+		fmt.Fprintf(w, "Request type other than GET or POST")
+	}
+
 }
--- FrontEnd/front-compi2/src/App.js
+++ FrontEnd/front-compi2/src/App.js
@@ -1,17 +1,45 @@
 import Editor from '@monaco-editor/react'
+import { useState, useRef } from 'react'
 
 export default function App() {
+  const [contentTranslate, setContentTranslate] = useState('')
+  const editorRef = useRef(null)
+  const handleEditorDidMount = (editor, monaco) =>{
+    editorRef.current = editor;
+  }
+
+  const handleTranslate = () => {
+    const content = editorRef.current.getValue()
+    const url = 'localhost:8080/texto'
+    //fetch hacia el back
+    const requestOptions = {
+      method: 'POST',
+      Headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({title: 'React POST Request Swift', data:content})
+    };
+
+    fetch(url,requestOptions   )
+      .then( response => response.json())
+      .then(data => this.setState({postId: data.id}));
+     
+
+  }
+
   return (
     <div>
+      <button onClick={handleTranslate}>Traducir</button>
       <div>
         <Editor
           height='50vh'
           theme='vs-dark'
           defaultLanguage='swift'
+          value={contentTranslate}
+          onChange={(value)=> setContentTranslate(value)}
+          onMount={handleEditorDidMount}
         />
       </div>
 
-      <div>
+      <div style={ {width: '50%', margin:'auto'}}>
         <Editor
           height='50vh'
           theme='vs-dark'
